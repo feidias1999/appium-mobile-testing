7f775058a9acc20b92dbccf28784bdfd2ae0f330
feidias1999 <feidiasa@gmail.com>
2026-05-04 20:30:46 +0300

    fixing spaces

--- src/test/java/feidiasantoniouacademy/AppiumBasics.java
+++ src/test/java/feidiasantoniouacademy/AppiumBasics.java
@@ -25,9 +25,9 @@ public class AppiumBasics extends BaseTest {
 		
 		//tagName[@attribute='value'] -> //tagName
 		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\'3. Preference dependencies\']")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
 		driver.findElement(By.id("android:id/checkbox")).click();
-		driver.findElement(By.xpath("(//android.widget.RelativeLayout) [2]")).click();
+		driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
 		//set wifi name
 		
 
