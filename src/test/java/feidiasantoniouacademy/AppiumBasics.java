b46f14ea90e6ae8567db20876bb565c948cb7c05
feidias1999 <feidiasa@gmail.com>
2026-05-04 20:14:50 +0300

    added accessibility Id and xpath for Preference Dependencies

--- src/test/java/feidiasantoniouacademy/AppiumBasics.java
+++ src/test/java/feidiasantoniouacademy/AppiumBasics.java
@@ -5,6 +5,7 @@ import java.net.MalformedURLException;
 
 import java.net.URISyntaxException;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -22,7 +23,11 @@ public class AppiumBasics extends BaseTest {
 		//Actual automation starts
 		//Xpath, id, accessibilityId, classname, androidUIAutomator
 		
+		//tagName[@attribute='value'] -> //tagName
 		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\'3. Preference dependencies\']")).click();
+		driver.findElement(By.id("android:id/checkbox")).click();
+		driver.findElement(By.xpath("(//android.widget.RelativeLayout) [2]")).click();
 		//set wifi name
 		
 
