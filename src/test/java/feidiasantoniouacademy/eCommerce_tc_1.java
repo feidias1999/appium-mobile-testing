4a8873a56eb4f3b727b16bd36515b64353ebf745
feidias1999 <feidiasa@gmail.com>
2026-05-06 23:46:13 +0300

    explicit wait on item on Cart

--- src/test/java/feidiasantoniouacademy/eCommerce_tc_1.java
+++ src/test/java/feidiasantoniouacademy/eCommerce_tc_1.java
@@ -1,6 +1,10 @@
 package feidiasantoniouacademy;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -32,8 +36,12 @@ public class eCommerce_tc_1 extends BaseTest {
 			}
 		}
 		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
-		Thread.sleep(3000);
+
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
 		
+		String lastPageProduct = driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText();
+		Assert.assertEquals(lastPageProduct, "Jordan 6 Rings");
 	}
 
 }
