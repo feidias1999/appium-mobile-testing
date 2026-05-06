ac6323cf42620e783e95f6f2a632ccfa17ade28f
feidias1999 <feidiasa@gmail.com>
2026-05-06 11:26:23 +0300

    Miscellaneous class create for landscape rotation

---
+++ src/test/java/feidiasantoniouacademy/MiscellaneousAppiumActions.java
@@ -0,0 +1,39 @@
+package feidiasantoniouacademy;
+
+
+import java.net.MalformedURLException;
+
+import java.net.URISyntaxException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumBy;
+
+
+@Test
+public class MiscellaneousAppiumActions extends BaseTest {
+	
+	public void Miscellaneous() throws MalformedURLException, URISyntaxException {
+	
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+		driver.findElement(By.id("android:id/checkbox")).click();
+		DeviceRotation landScape = new DeviceRotation(0, 0, 90);
+		driver.rotate(landScape);
+		driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+		String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
+		Assert.assertEquals(alertTitle, "WiFi settings");
+		driver.findElement(By.id("android:id/edit")).sendKeys("Feidias Wifi");
+		driver.findElements(AppiumBy.className("android.widget.Button")).get(1).click();
+		//set wifi name
+		
+
+	
+		
+	}
+s
+}
+ 
