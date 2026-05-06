12bf07e58649acc82472ef9799c5ab7310984712
feidias1999 <feidiasa@gmail.com>
2026-05-06 23:08:44 +0300

    assertion test passed successfully

--- src/test/java/feidiasantoniouacademy/eCommerce_tc_1.java
+++ src/test/java/feidiasantoniouacademy/eCommerce_tc_1.java
@@ -1,6 +1,7 @@
 package feidiasantoniouacademy;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumBy;
@@ -10,15 +11,15 @@ public class eCommerce_tc_1 extends BaseTest {
 	@Test
 	public void FillForm() throws InterruptedException {
 		
-		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Feidias Antoniou");
+//		driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Feidias Antoniou");
 		driver.hideKeyboard();
 		driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
 		driver.findElement(By.id("android:id/text1")).click();
 		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));"));
 		driver.findElement(By.xpath("//android.widget.TextView[@text='Argentina']")).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
-		Thread.sleep(3000);
-
+		String toastMessage = driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name");
+		Assert.assertEquals(toastMessage, "Please enter your name");
 	}
 
 }
