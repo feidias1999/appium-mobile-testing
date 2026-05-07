094f12f3ae67d49c9aab66cc8e4af33bf918ef7e
feidias1999 <feidiasa@gmail.com>
2026-05-07 17:20:49 +0300

    longPress button and then click and proceed tc - passed

--- src/test/java/feidiasantoniouacademy/eCommerce_tc_3.java
+++ src/test/java/feidiasantoniouacademy/eCommerce_tc_3.java
@@ -28,6 +28,7 @@ public class eCommerce_tc_3 extends BaseTest {
 		driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
 		driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
 //		driver.findElement(By.xpath("(//android.widget.TextView[@text='ADD TO CART'])[1]")).click();
+		Thread.sleep(2000);
 		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
 		wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
 		List<WebElement> productPrices = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice"));
@@ -45,9 +46,12 @@ public class eCommerce_tc_3 extends BaseTest {
 		String displaySum= driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText();
 		Double displayFormattedSum = getFormattedAmount(displaySum);
 		Assert.assertEquals(totalSum, displayFormattedSum);
-		
-		
-		
+		WebElement ele = driver.findElement(By.id("com.androidsample.generalstore:id/termsButton"));
+		longPressAction(ele);
+		driver.findElement(By.id("android:id/button1")).click();
+		driver.findElement(AppiumBy.className("android.widget.CheckBox")).click();
+		driver.findElement(By.id("com.androidsample.generalstore:id/btnProceed")).click();
+		Thread.sleep(2000);
 	}
 
 }
