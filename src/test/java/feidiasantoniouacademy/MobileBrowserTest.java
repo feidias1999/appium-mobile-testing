a411fd0ac724937418b42a092a93850c4a3404a1
feidias1999 <feidiasa@gmail.com>
2026-05-08 18:10:57 +0300

    Assertion created

--- src/test/java/feidiasantoniouacademy/MobileBrowserTest.java
+++ src/test/java/feidiasantoniouacademy/MobileBrowserTest.java
@@ -1,19 +1,28 @@
 package feidiasantoniouacademy;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import com.google.common.collect.ImmutableMap;
 
 public class MobileBrowserTest extends BrowserBaseTest {
 	
 	@Test
 	public void browserTest() {
 		
-		driver.get("http://google.com");
-		System.out.println(driver.getTitle());
-		driver.findElement(By.name("q")).sendKeys("Feidias Antoniou Academy");
-		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-
+//		driver.get("http://google.com");
+//		System.out.println(driver.getTitle());
+//		driver.findElement(By.name("q")).sendKeys("Feidias Antoniou Academy");
+//		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.xpath("//span[@class='navbar-toggler-icon']")).click();
+		driver.findElement(By.cssSelector("a[routerlink*='products']")).click();
+		((JavascriptExecutor) driver).executeScript("window.scrollBy(0.1000)", "");
+		String text = driver.findElement(By.cssSelector("a[href*='products/3']")).getText();
+		Assert.assertEquals(text, "Devops");
 	}
 
 }
