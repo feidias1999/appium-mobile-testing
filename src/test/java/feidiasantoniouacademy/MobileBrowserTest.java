478c4730c0375ad5a6682c70a38a6c4d7a6c3e07
feidias1999 <feidiasa@gmail.com>
2026-05-08 17:46:55 +0300

    MobileBrowserTest class created

---
+++ src/test/java/feidiasantoniouacademy/MobileBrowserTest.java
@@ -0,0 +1,19 @@
+package feidiasantoniouacademy;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+
+public class MobileBrowserTest extends BrowserBaseTest {
+	
+	@Test
+	public void browserTest() {
+		
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.name("q")).sendKeys("Feidias Antoniou Academy");
+		driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+
+	}
+
+}
